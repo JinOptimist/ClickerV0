8211748abf6dc7a407ebcf46361c110de96a301a
JinOptimist <88jin@mail.ru>
2023-08-18 10:39:41 +0300

    Remove all except HomeController

--- ClickerWeb/Controllers/HomeController.cs
+++ ClickerWeb/Controllers/HomeController.cs
@@ -1,7 +1,4 @@
-﻿using ClickerWeb.DAL;
-using ClickerWeb.DAL.Models;
-using ClickerWeb.Models;
-using Microsoft.AspNetCore.Authorization;
+﻿using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
 
@@ -9,40 +6,9 @@ namespace ClickerWeb.Controllers
 {
     public class HomeController : Controller
     {
-        private UserManager<User> _userManager;
-        private WebContext _webContext;
-
-        public HomeController(UserManager<User> userManager, WebContext webContext)
-        {
-            _userManager = userManager;
-            _webContext = webContext;
-        }
-
-        [Authorize]
         public IActionResult Index()
         {
-            var user = _userManager.FindByNameAsync(User.Identity?.Name).Result;
-            var viewModel = new IndexViewModel();
-
-            viewModel.UserName = user.UserName;
-            viewModel.Exp = user.Exp;
-            viewModel.Coins = user.Coins;
-            viewModel.CurrentLevelName = user.CurrentLevel.Name;
-
-            viewModel.LevelDetails = _webContext
-                .LevelRules
-                .Where(x => x.MinExp <= user.Exp)
-                .Select(x => new LevelDetailViewModel
-                {
-                    Name = x.Name,
-                    Level = x.Level,
-                    ExpSalaryRate = x.ExpSalaryRate,
-                    LearningStepSize = x.LearningStepSize,
-                    ImageUrl = x.LevelImageUrl
-                })
-                .ToList();
-
-            return View(viewModel);
+            return View();
         }
     }
 }
--- ClickerWeb/Program.cs
+++ ClickerWeb/Program.cs
@@ -1,39 +1,9 @@
-using ClickerWeb.DAL;
-using ClickerWeb.DAL.Models;
-using Microsoft.AspNetCore.Identity;
-using Microsoft.EntityFrameworkCore;
-
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
-var connectionString = "Server=(localdb)\\MSSQLLocalDB;Database=ClickerV0;Trusted_Connection=True;";
-builder.Services
-    .AddDbContext<WebContext>(option =>
-        option
-            .UseLazyLoadingProxies(true)
-            .UseSqlServer(connectionString));
-
-builder.Services
-    .AddIdentity<User, IdentityRole>()
-    .AddEntityFrameworkStores<WebContext>();
-
-builder.Services.AddCors(option =>
-{
-    option.AddDefaultPolicy(policy => 
-        policy.WithOrigins("http://localhost:3000"));
-});
-
-builder.Services.ConfigureApplicationCookie(option =>
-{
-    option.LoginPath = "/Account/Login";
-});
-
 builder.Services.AddControllersWithViews();
 
 var app = builder.Build();
-
-var seedDatabase = new SeedDatabase();
-seedDatabase.Seed(app.Services);
 
 // Configure the HTTP request pipeline.
 if (!app.Environment.IsDevelopment())
